0008b8630292137a23fa8a220d9cd006846d1a91
Andryss <andr.krivosheev@gmail.com>
2025-12-10 22:50:01 +0300

    IoT controller initial commit

---
+++ iot-controller/src/test/java/ru/itmo/mopsync/iotcontroller/IotControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.mopsync.iotcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
