a196632b091284cb974a0862d568b4d0607c32ab
Andryss <andr.krivosheev@gmail.com>
2025-12-10 22:56:13 +0300

    Rule engine initial commit

---
+++ rule-engine/src/test/java/ru/itmo/mopsync/ruleengine/RuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.mopsync.ruleengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
