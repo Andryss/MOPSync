0dfc89d9e9ff37ae8f832329800a846e4436420e
Andryss <andr.krivosheev@gmail.com>
2025-12-11 15:44:59 +0300

    Data simulator initial commit

---
+++ data-simulator/src/main/java/ru/itmo/mopsync/datasimulator/DataSimulatorApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.mopsync.datasimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSimulatorApplication.class, args);
+    }
+
+}
---
+++ data-simulator/src/test/java/ru/itmo/mopsync/datasimulator/DataSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.mopsync.datasimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
